f4de0610606fab7dc610d1e1fe138a7c3da3961b
duesacrifice69 <chirathtimodh@gmail.com>
2023-08-02 06:49:16 +0530

    init comit: all vacancies page added

---
+++ client/src/Pages/Vacancies/VacancyList.jsx
@@ -0,0 +1,159 @@
+import {
+  Paper,
+  Container,
+  Typography,
+  InputBase,
+  IconButton,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import { Search } from "@mui/icons-material";
+import { useTheme } from "@mui/material/styles";
+import Vacancy from "../../components/Vacancy";
+
+const vacancies = [
+  {
+    vacancyId: 1234,
+    title: "Deputy General Manager",
+    status: "Open",
+    recType: "External Recrutiement",
+    closingDate: "2023 sep 20",
+    slaryGroup: "HM 1-1",
+    boardGrade: "G2",
+    advertisement: "/path",
+    NoOfApplied: "7",
+    postedDays: 2,
+  },
+  {
+    vacancyId: 5678,
+    title: "Assistant General Manager",
+    status: "Open",
+    recType: "External Recrutiement",
+    closingDate: "2023 sep 20",
+    slaryGroup: "HM 1-1",
+    boardGrade: "G2",
+    advertisement: "/path",
+    NoOfApplied: "7",
+    postedDays: 2,
+  },
+  {
+    vacancyId: 8498,
+    title: "General Manager",
+    status: "Open",
+    recType: "External Recrutiement",
+    closingDate: "2023 sep 20",
+    slaryGroup: "HM 1-1",
+    boardGrade: "G2",
+    advertisement: "/path",
+    NoOfApplied: "7",
+    postedDays: 2,
+  },
+  {
+    vacancyId: 9101,
+    title: "Deputy General Manager",
+    status: "Open",
+    recType: "External Recrutiement",
+    closingDate: "2023 sep 20",
+    slaryGroup: "HM 1-1",
+    boardGrade: "G2",
+    advertisement: "/path",
+    NoOfApplied: "7",
+    postedDays: 2,
+  },
+  {
+    vacancyId: 1233,
+    title: "Deputy General Manager",
+    status: "Open",
+    recType: "External Recrutiement",
+    closingDate: "2023 sep 20",
+    slaryGroup: "HM 1-1",
+    boardGrade: "G2",
+    advertisement: "/path",
+    NoOfApplied: "7",
+    postedDays: 2,
+  },
+];
+
+const VacancyList = () => {
+  const [setIsNavBar, setActive] = useOutletContext();
+  const [vacancyList, setVacancyList] = useState(vacancies);
+  const [searchText, setSearchText] = useState("");
+  const theme = useTheme();
+
+  useEffect(() => setIsNavBar(true), []);
+  useEffect(() => setActive("2"), []);
+
+  const handleSearch = (vacancies, query) => {
+    const filteredVacancies = vacancies.filter((vacancy) =>
+      vacancy.title
+        .toLowerCase()
+        .split(" ")
+        .some((word) => word.startsWith(...query.toLowerCase().split(" ")))
+    );
+    setVacancyList(query.length > 0 ? filteredVacancies : vacancies);
+  };
+
+  return (
+    <div style={{ backgroundColor: theme.palette.background.main }}>
+      <Container component="main" maxWidth="md">
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Typography
+            sx={{
+              fontWeight: 700,
+              fontSize: "18px",
+              mb: "1rem",
+            }}
+          >
+            Available Opportunities
+          </Typography>
+          <Paper
+            component="form"
+            sx={{
+              p: "2px 4px",
+              display: "flex",
+              alignItems: "center",
+              width: "75%",
+              backgroundcolor: theme.palette.background.main,
+            }}
+          >
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              onChange={(e) => setSearchText(e.target.value)}
+              onKeyPress={(e) => {
+                if (e.key === "Enter") {
+                  e.preventDefault();
+                  handleSearch(vacancies, searchText);
+                }
+              }}
+              placeholder="Search..."
+            />
+            <IconButton onClick={() => handleSearch(vacancies, searchText)}>
+              <Search />
+            </IconButton>
+          </Paper>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "1rem",
+            marginTop: "2rem",
+          }}
+        >
+          {vacancyList.map((detail, i) => (
+            <Vacancy key={i} detail={detail} />
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default VacancyList;
---
+++ client/src/components/Vacancy.jsx
@@ -0,0 +1,157 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Typography,
+  Grid,
+  useTheme,
+  useMediaQuery,
+  IconButton,
+} from "@mui/material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import { useNavigate } from "react-router-dom";
+
+const Vacancy = ({ detail, onDelete, onEdit }) => {
+  const subTitle = `Posted ${detail.postedDays} days ago`;
+  const isMobile = useMediaQuery("(max-width: 600px)");
+  const theme = useTheme();
+  const navigate = useNavigate();
+
+  const handleApply = () => {
+    navigate("/application/basicDetails", {
+      state: { vacancyId: detail.vacancyId, vacancy: detail.title },
+    });
+  };
+
+  return (
+    <Card sx={{ width: "100%" }}>
+      <CardHeader
+        title={detail.title}
+        subheader={subTitle}
+        sx={{ backgroundColor: theme.palette.secondary[400], p: "12px" }}
+        titleTypographyProps={{
+          fontSize: (isMobile) => (isMobile ? "1.2rem" : "1.5rem"),
+          fontWeight: 600,
+        }}
+        subheaderTypographyProps={{
+          fontSize: (isMobile) => (isMobile ? "0.8rem" : "1rem"),
+        }}
+      />
+      <CardContent>
+        <Typography
+          sx={{
+            fontSize: (isMobile) => (isMobile ? "14px" : "16px"),
+            fontWeight: 550,
+            mb: "10px",
+            color: theme.palette.secondary[800],
+          }}
+        >
+          {detail.recType}
+        </Typography>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid item xs={isMobile ? 12 : 4}>
+            <div
+              style={{
+                display: "flex",
+                gap: "10px",
+              }}
+            >
+              <Typography fontSize={isMobile ? "14px" : "16px"}>
+                Closing date:{" "}
+              </Typography>
+              <Typography
+                fontSize={isMobile ? "14px" : "16px"}
+                sx={{ fontWeight: 550, color: theme.palette.secondary[700] }}
+              >
+                {detail.closingDate}
+              </Typography>
+            </div>
+          </Grid>
+          <Grid item xs={isMobile ? 12 : 8}>
+            <div style={{ display: "flex", gap: "10px" }}>
+              <Typography fontSize={isMobile ? "14px" : "16px"}>
+                Published date:{" "}
+              </Typography>
+              <Typography
+                fontSize={isMobile ? "14px" : "16px"}
+                sx={{ fontWeight: 550, color: theme.palette.secondary[700] }}
+              >
+                {detail.closingDate}
+              </Typography>
+            </div>
+          </Grid>
+          <Grid item xs={isMobile ? 12 : 4}>
+            <div style={{ display: "flex", gap: "10px" }}>
+              <Typography fontSize={isMobile ? "14px" : "16px"}>
+                Salary Group:{" "}
+              </Typography>
+              <Typography
+                fontSize={isMobile ? "14px" : "16px"}
+                sx={{ fontWeight: 550, color: theme.palette.secondary[700] }}
+              >
+                {detail.slaryGroup}
+              </Typography>
+            </div>
+          </Grid>
+          <Grid item xs={isMobile ? 12 : 4}>
+            <div style={{ display: "flex", gap: "10px" }}>
+              <Typography fontSize={isMobile ? "14px" : "16px"}>
+                Board Grade:{" "}
+              </Typography>
+              <Typography
+                fontSize={isMobile ? "14px" : "16px"}
+                sx={{ fontWeight: 550, color: theme.palette.secondary[700] }}
+              >
+                {detail.boardGrade}
+              </Typography>
+            </div>
+          </Grid>
+          <Grid item xs={isMobile ? 12 : 4}>
+            <div style={{ display: "flex", gap: "10px" }}>
+              <Typography fontSize={isMobile ? "14px" : "16px"}>
+                Advertisement:{" "}
+              </Typography>
+              <DescriptionOutlinedIcon
+                sx={{ color: theme.palette.primary[500] }}
+              />
+            </div>
+          </Grid>
+        </Grid>
+        <hr
+          style={{
+            border: "none",
+            height: "2px",
+            backgroundColor: theme.palette.secondary[300],
+          }}
+        />
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ display: "flex", gap: "10px" }}>
+            <VisibilityIcon sx={{ fontSize: "24px" }} />
+            <Typography fontSize={isMobile ? "14px" : "16px"}>
+              Applied ({detail.NoOfApplied})
+            </Typography>
+          </div>
+          <div style={{ marginLeft: "auto" }}>
+            <IconButton
+              sx={{ color: theme.palette.secondary[900] }}
+              onClick={onDelete}
+            >
+              <DeleteIcon />
+            </IconButton>
+            <IconButton
+              sx={{ color: theme.palette.secondary[900] }}
+              onClick={onEdit}
+            >
+              <EditNoteIcon />
+            </IconButton>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Vacancy;
