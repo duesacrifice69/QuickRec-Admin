1e7dfa882d106a3e4eeb95aa89717e5525db9b2c
duesacrifice <chirathtimodh@gmail.com>
2023-08-07 15:56:17 +0530

    navigation menu for mobile added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ import {
   IconButton,
   ListItem,
   Typography,
+  Drawer,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
@@ -23,11 +24,22 @@ import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import Fade from "@mui/material/Fade";
 
-const Navbar = ({ active, isSidebarOpen, setIsSidebarOpen, isNonMobile }) => {
+const Navbar = ({
+  active,
+  isSidebarOpen,
+  setIsSidebarOpen,
+  isNonMobile,
+  window,
+}) => {
   const theme = useTheme();
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = useState(false);
+  const open = Boolean(anchorEl);
   const user = useSelector((state) => state.userContext.data);
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   useEffect(() => {
     if (!user?.result) {
@@ -41,14 +53,13 @@ const Navbar = ({ active, isSidebarOpen, setIsSidebarOpen, isNonMobile }) => {
     localStorage.clear();
     dispatch(logOut());
   };
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
   const handleMenuClick = (event) => {
-    setAnchorEl(event.currentTarget);
+    isNonMobile ? setAnchorEl(event.currentTarget) : setAnchorEl(!anchorEl);
   };
-  const handleMenuItemClick = (e, navigateTo) => {
+  const handleClick = (e, navigateTo) => {
     e.stopPropagation();
-    setAnchorEl(null);
+    isNonMobile ? setAnchorEl(null) : setAnchorEl(false);
+    !isNonMobile && setIsSidebarOpen(false);
     navigateTo && navigate(navigateTo);
   };
   return (
@@ -80,6 +91,109 @@ const Navbar = ({ active, isSidebarOpen, setIsSidebarOpen, isNonMobile }) => {
               >
                 <MenuIcon />
               </IconButton>
+              <Drawer
+                container={container}
+                variant="temporary"
+                open={isSidebarOpen}
+                onClose={() => {
+                  setAnchorEl(false);
+                  setIsSidebarOpen(!isSidebarOpen);
+                }}
+                ModalProps={{
+                  keepMounted: true, // Better open performance on mobile.
+                }}
+                sx={{
+                  display: { xs: "block", sm: "none" },
+                  "& .MuiDrawer-paper": {
+                    boxSizing: "border-box",
+                    backgroundColor: theme.palette.secondary.main,
+                  },
+                }}
+              >
+                <ListItem role="none">
+                  <ListItemStyle
+                    key="1"
+                    onClick={(e) => handleClick(e, "/home")}
+                    sx={{
+                      fontSize: "16px",
+                      fontWeight: 400,
+                    }}
+                  >
+                    Home
+                  </ListItemStyle>
+                </ListItem>
+
+                <ListItem role="none">
+                  <ListItemStyle
+                    key="2"
+                    onClick={handleMenuClick}
+                    sx={{
+                      fontSize: "16px",
+                      fontWeight: 400,
+                    }}
+                  >
+                    Vacancy
+                    <KeyboardArrowDownIcon sx={{ pl: "5px" }} />
+                  </ListItemStyle>
+                </ListItem>
+                {anchorEl && (
+                  <span
+                    style={{ backgroundColor: theme.palette.secondary[400] }}
+                  >
+                    <ListItem role="none">
+                      <ListItemStyle
+                        key="3"
+                        onClick={(e) => handleClick(e, "/postVacancy")}
+                        sx={{
+                          fontSize: "14px",
+                          fontWeight: 400,
+                        }}
+                      >
+                        Post Vacancy
+                      </ListItemStyle>
+                    </ListItem>
+                    <ListItem role="none">
+                      <ListItemStyle
+                        key="4"
+                        onClick={(e) => handleClick(e, "/vacancies")}
+                        sx={{
+                          fontSize: "14px",
+                          fontWeight: 400,
+                        }}
+                      >
+                        All Vacancies
+                      </ListItemStyle>
+                    </ListItem>
+                  </span>
+                )}
+
+                <ListItem role="none">
+                  <ListItemStyle
+                    key="5"
+                    onClick={handleClick}
+                    sx={{
+                      fontSize: "16px",
+                      fontWeight: 400,
+                    }}
+                  >
+                    Profile
+                  </ListItemStyle>
+                </ListItem>
+
+                <ListItem role="none">
+                  <ListItemStyle
+                    key="6"
+                    onClick={handleLogOut}
+                    sx={{
+                      fontSize: "16px",
+                      fontWeight: 400,
+                      width: "max-content",
+                    }}
+                  >
+                    Log out
+                  </ListItemStyle>
+                </ListItem>
+              </Drawer>
             </Box>
           ) : (
             <Box component="nav">
@@ -87,9 +201,7 @@ const Navbar = ({ active, isSidebarOpen, setIsSidebarOpen, isNonMobile }) => {
                 <ListItem role="none">
                   <ListItemStyle
                     key="1"
-                    onClick={() => {
-                      navigate("/home");
-                    }}
+                    onClick={(e) => handleClick(e, "/home")}
                     sx={{
                       borderBottom: `5px solid ${
                         active === "1"
@@ -141,17 +253,13 @@ const Navbar = ({ active, isSidebarOpen, setIsSidebarOpen, isNonMobile }) => {
                       }}
                       anchorEl={anchorEl}
                       open={open}
-                      onClose={(e) => handleMenuItemClick(e)}
+                      onClose={(e) => handleClick(e)}
                       TransitionComponent={Fade}
                     >
-                      <MenuItem
-                        onClick={(e) => handleMenuItemClick(e, "/postVacancy")}
-                      >
+                      <MenuItem onClick={(e) => handleClick(e, "/postVacancy")}>
                         Post Vacancy
                       </MenuItem>
-                      <MenuItem
-                        onClick={(e) => handleMenuItemClick(e, "/vacancies")}
-                      >
+                      <MenuItem onClick={(e) => handleClick(e, "/vacancies")}>
                         All Vacancies
                       </MenuItem>
                     </Menu>
@@ -184,7 +292,7 @@ const Navbar = ({ active, isSidebarOpen, setIsSidebarOpen, isNonMobile }) => {
                           ? theme.palette.primary[500]
                           : theme.palette.secondary.main
                       }`,
-                      width: "120px",
+                      width: "max-content",
                     }}
                   >
                     Log out
