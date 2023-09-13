e33b0787736be2f0a60081e99a9dda3036bf8f41
duesacrifice <chirathtimodh@gmail.com>
2023-09-13 15:22:24 +0530

    report download added

--- client/src/Pages/PostVacancy/PostVacany.jsx
+++ client/src/Pages/PostVacancy/PostVacany.jsx
@@ -5,6 +5,9 @@ import {
   Box,
   Container,
   Grid,
+  List,
+  ListItem,
+  ListItemButton,
   Paper,
   Switch,
   Typography,
@@ -18,6 +21,7 @@ import {
   useGetMasterDataQuery,
 } from "../../state/api";
 import Error from "../../components/Error";
+import { downloadHandler } from "../../components/DownloadIcon";
 
 const initState = {
   VacancyName: "",
@@ -96,6 +100,14 @@ const PostVacancy = ({ isEditing, setIsEditing, editingVacancy }) => {
 
   const handleCancel = () => setIsEditing(false);
 
+  const handleReportSchedule = () => {
+    downloadHandler({
+      fileName: "RPTScheduleDetails.pdf",
+      path: "/report/ApplicantsReport",
+      body: { vacancyId: vacancy?.VacancyId },
+    });
+  };
+
   return (
     <Box
       sx={{
@@ -103,7 +115,7 @@ const PostVacancy = ({ isEditing, setIsEditing, editingVacancy }) => {
         pb: "3rem",
       }}
     >
-      <Container maxWidth="md">
+      <Container maxWidth="lg">
         <div
           style={{
             display: "flex",
@@ -130,73 +142,75 @@ const PostVacancy = ({ isEditing, setIsEditing, editingVacancy }) => {
                 </Typography>
               </Grid>
             </Grid>
-            <Paper
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                p: "2rem 3vw",
-                width: "100%",
-              }}
-            >
-              <Grid container rowSpacing={3}>
-                <Grid item xs={12}>
-                  <Typography
-                    sx={{
-                      fontWeight: 600,
-                      fontSize: "1.1rem",
-                      mb: "1rem",
-                    }}
-                  >
-                    {vacancy.RecruitmentType &&
-                      recruitmentOptions.find(
-                        (v) => v.value === vacancy.RecruitmentType
-                      ).text + " to : "}
-                    <span
-                      style={{
-                        color: theme.palette.primary[500],
-                      }}
-                    >
-                      {vacancy.VacancyName}
-                    </span>
-                  </Typography>
-                </Grid>
-                <Input
-                  name="VacancyName"
-                  value={vacancy.VacancyName}
-                  handleChange={handleChange}
-                  label="Vacancy :"
-                  required
-                  inline
-                />
-                <Input
-                  name="RecruitmentType"
-                  type="select"
-                  options={recruitmentOptions}
-                  value={vacancy.RecruitmentType}
-                  handleChange={handleChange}
-                  label="Recruitment Method :"
-                  required
-                  inline
-                />
-                <Input
-                  name="ClosingDate"
-                  type="date"
-                  value={vacancy.ClosingDate}
-                  handleChange={handleChange}
-                  label="Closing Date of Application :"
-                  required
-                  inline
-                />
-                <Input
-                  name="PlannedInterViewDate"
-                  type="date"
-                  value={vacancy.PlannedInterViewDate}
-                  handleChange={handleChange}
-                  label="Expected Date of Interview :"
-                  required
-                  inline
-                />
-                {/* <Input
+            <div style={{ display: "flex" }}>
+              <Container maxWidth="md">
+                <Paper
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    p: "2rem 3vw",
+                    width: "100%",
+                  }}
+                >
+                  <Grid container rowSpacing={3}>
+                    <Grid item xs={12}>
+                      <Typography
+                        sx={{
+                          fontWeight: 600,
+                          fontSize: "1.1rem",
+                          mb: "1rem",
+                        }}
+                      >
+                        {vacancy.RecruitmentType &&
+                          recruitmentOptions.find(
+                            (v) => v.value === vacancy.RecruitmentType
+                          ).text + " to : "}
+                        <span
+                          style={{
+                            color: theme.palette.primary[500],
+                          }}
+                        >
+                          {vacancy.VacancyName}
+                        </span>
+                      </Typography>
+                    </Grid>
+                    <Input
+                      name="VacancyName"
+                      value={vacancy.VacancyName}
+                      handleChange={handleChange}
+                      label="Vacancy :"
+                      required
+                      inline
+                    />
+                    <Input
+                      name="RecruitmentType"
+                      type="select"
+                      options={recruitmentOptions}
+                      value={vacancy.RecruitmentType}
+                      handleChange={handleChange}
+                      label="Recruitment Method :"
+                      required
+                      inline
+                    />
+                    <Input
+                      name="ClosingDate"
+                      type="date"
+                      value={vacancy.ClosingDate}
+                      handleChange={handleChange}
+                      label="Closing Date of Application :"
+                      required
+                      inline
+                    />
+                    <Input
+                      name="PlannedInterViewDate"
+                      type="date"
+                      value={vacancy.PlannedInterViewDate}
+                      handleChange={handleChange}
+                      label="Expected Date of Interview :"
+                      required
+                      inline
+                    />
+                    {/* <Input
                   type="number"
                   name="ExpectedNoOfApplicants"
                   label="Expected No. of Applicants :"
@@ -205,120 +219,155 @@ const PostVacancy = ({ isEditing, setIsEditing, editingVacancy }) => {
                   required
                   inline
                 /> */}
-                <Input
-                  type="number"
-                  name="NoOfVacancies"
-                  label="No. of Vacancies :"
-                  value={vacancy.NoOfVacancies}
-                  handleChange={handleChange}
-                  inline
-                  required
-                />
-                <Input
-                  type="number"
-                  name="AgeLimit"
-                  label="Age Limit :"
-                  value={vacancy.AgeLimit}
-                  handleChange={handleChange}
-                  inline
-                  required
-                />
-                <Input
-                  name="AdvertismentPath"
-                  label="Advertisment :"
-                  value={vacancy.AdvertismentPath}
-                  setAttachment={setAttachment}
-                  type="file"
-                  handleChange={handleChange}
-                  inline
-                />
-                <Input
-                  name="Remarks"
-                  label="Remarks :"
-                  value={vacancy.Remarks}
-                  handleChange={handleChange}
-                  size="medium"
-                  multiline
-                  inline
-                  minRows={5}
-                  maxRows={8}
-                />
-                <Grid item xs={isMobile ? 12 : 6}>
-                  <Typography sx={{ pt: "0.5rem", fontWeight: 500 }}>
-                    Active :
-                  </Typography>
-                </Grid>
-                <Grid item xs={isMobile ? 12 : 6}>
-                  <Switch
-                    checked={vacancy.Status === "ACT" ? true : false}
-                    onChange={(e) => {
-                      handleChange({
-                        target: {
-                          name: "Status",
-                          value: e.target.checked === true ? "ACT" : "INA",
-                        },
-                      });
+                    <Input
+                      type="number"
+                      name="NoOfVacancies"
+                      label="No. of Vacancies :"
+                      value={vacancy.NoOfVacancies}
+                      handleChange={handleChange}
+                      inline
+                      required
+                    />
+                    <Input
+                      type="number"
+                      name="AgeLimit"
+                      label="Age Limit :"
+                      value={vacancy.AgeLimit}
+                      handleChange={handleChange}
+                      inline
+                      required
+                    />
+                    <Input
+                      name="AdvertismentPath"
+                      label="Advertisment :"
+                      value={vacancy.AdvertismentPath}
+                      setAttachment={setAttachment}
+                      type="file"
+                      handleChange={handleChange}
+                      inline
+                    />
+                    <Input
+                      name="Remarks"
+                      label="Remarks :"
+                      value={vacancy.Remarks}
+                      handleChange={handleChange}
+                      size="medium"
+                      multiline
+                      inline
+                      minRows={5}
+                      maxRows={8}
+                    />
+                    <Grid item xs={isMobile ? 12 : 6}>
+                      <Typography sx={{ pt: "0.5rem", fontWeight: 500 }}>
+                        Active :
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={isMobile ? 12 : 6}>
+                      <Switch
+                        checked={vacancy.Status === "ACT" ? true : false}
+                        onChange={(e) => {
+                          handleChange({
+                            target: {
+                              name: "Status",
+                              value: e.target.checked === true ? "ACT" : "INA",
+                            },
+                          });
+                        }}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <hr />
+                    </Grid>
+                    <Input
+                      name="SalaryGroupId"
+                      label="Salary Group :"
+                      type="select"
+                      value={vacancy.SalaryGroupId}
+                      handleChange={handleChange}
+                      options={masterData?.data?.salaryGroups}
+                      loading={masterDataIsLoading}
+                      autocomplete
+                      inline
+                      required
+                    />
+                    <Input
+                      name="BoardGradeId"
+                      label="Board Grade :"
+                      type="select"
+                      options={masterData?.data?.boardGrades}
+                      loading={masterDataIsLoading}
+                      value={vacancy.BoardGradeId}
+                      handleChange={handleChange}
+                      autocomplete
+                      inline
+                      required
+                    />
+                    <Grid item xs={12}>
+                      {!isEditing ? (
+                        <ButtonComp
+                          type="submit"
+                          sx={{
+                            display: "block",
+                            m: "2rem 0",
+                            ml: "auto",
+                            p: "1rem ",
+                          }}
+                        >
+                          Post Vacancy
+                        </ButtonComp>
+                      ) : (
+                        <div
+                          style={{
+                            display: "flex",
+                            justifyContent: "right",
+                            gap: "10px",
+                          }}
+                        >
+                          <ButtonComp onClick={handleCancel}>Cancel</ButtonComp>
+                          <ButtonComp type="submit">Save</ButtonComp>
+                        </div>
+                      )}
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Error error={error} setError={setError} />
+                    </Grid>
+                  </Grid>
+                </Paper>
+              </Container>
+              {isEditing && (
+                <Paper>
+                  <Typography
+                    sx={{
+                      textAlign: "center",
+                      m: "1rem auto",
+                      fontWeight: 500,
                     }}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <hr />
-                </Grid>
-                <Input
-                  name="SalaryGroupId"
-                  label="Salary Group :"
-                  type="select"
-                  value={vacancy.SalaryGroupId}
-                  handleChange={handleChange}
-                  options={masterData?.data?.salaryGroups}
-                  loading={masterDataIsLoading}
-                  autocomplete
-                  inline
-                  required
-                />
-                <Input
-                  name="BoardGradeId"
-                  label="Board Grade :"
-                  type="select"
-                  options={masterData?.data?.boardGrades}
-                  loading={masterDataIsLoading}
-                  value={vacancy.BoardGradeId}
-                  handleChange={handleChange}
-                  autocomplete
-                  inline
-                  required
-                />
-                <Grid item xs={12}>
-                  {!isEditing ? (
-                    <ButtonComp
-                      type="submit"
-                      sx={{
-                        display: "block",
-                        m: "2rem 0",
-                        ml: "auto",
-                        p: "1rem ",
-                      }}
-                    >
-                      Post Vacancy
-                    </ButtonComp>
-                  ) : (
-                    <div
-                      style={{
-                        display: "flex",
-                        justifyContent: "right",
-                        gap: "10px",
-                      }}
-                    >
-                      <ButtonComp onClick={handleCancel}>Cancel</ButtonComp>
-                      <ButtonComp type="submit">Save</ButtonComp>
-                    </div>
-                  )}
-                </Grid>
-                <Grid item xs={12}>
-                  <Error error={error} setError={setError} />
-                </Grid>
-              </Grid>
-            </Paper>
+                  >
+                    Documents
+                  </Typography>
+                  <List
+                    sx={{
+                      "& .MuiButtonBase-root": {
+                        borderBottom: (theme) =>
+                          "1px solid " + theme.palette.secondary[600],
+                      },
+                      "& li:last-child .MuiButtonBase-root": {
+                        borderBottom: "none",
+                      },
+                    }}
+                  >
+                    <ListItem>
+                      <ListItemButton onClick={handleReportSchedule}>
+                        Report Schedule
+                      </ListItemButton>
+                    </ListItem>
+                    <ListItem>
+                      <ListItemButton>Advertisement</ListItemButton>
+                    </ListItem>
+                  </List>
+                </Paper>
+              )}
+            </div>
           </form>
         </div>
       </Container>
--- client/src/components/DownloadIcon.jsx
+++ client/src/components/DownloadIcon.jsx
@@ -1,36 +1,39 @@
 import { IconButton, useTheme } from "@mui/material";
 import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
 
+export const downloadHandler = async ({ fileName, path, body }) => {
+  let res = await fetch(process.env.REACT_APP_BASE_URL + path, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  let blob = await res.blob();
+  const data = window.URL.createObjectURL(new Blob([blob]));
+  const filename = res.headers.get("Content-Disposition");
+
+  const link = document.createElement("a");
+  link.href = data;
+  link.setAttribute("download", filename ?? fileName);
+  document.body.appendChild(link);
+  link.click();
+  link.parentNode?.removeChild(link);
+};
 const DownloadIcon = ({ fileName }) => {
   const theme = useTheme();
 
-  const downloadHandler = async (fileName) => {
-    let res = await fetch(
-      process.env.REACT_APP_BASE_URL + "/download/pdfDownload",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          fileName: fileName,
-        }),
-      }
-    );
-    let blob = await res.blob();
-    const data = window.URL.createObjectURL(new Blob([blob]));
-
-    const link = document.createElement("a");
-    link.href = data;
-    link.setAttribute("download", fileName);
-    document.body.appendChild(link);
-    link.click();
-    link.parentNode?.removeChild(link);
-  };
-
   return (
     <IconButton
-      onClick={() => downloadHandler(fileName)}
+      onClick={() =>
+        downloadHandler({
+          fileName: fileName,
+          path: "/download/pdfDownload",
+          body: {
+            fileName: fileName,
+          },
+        })
+      }
       disabled={fileName ? false : true}
       sx={{
         color: theme.palette.primary[500],
