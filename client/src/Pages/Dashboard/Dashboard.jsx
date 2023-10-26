9355be0e1acb28aead463934c4431b5778419e28
duesacrifice69 <chirathtimodh@gmail.com>
2023-10-26 06:05:26 +0530

    barchart added

--- client/src/Pages/Dashboard/Dashboard.jsx
+++ client/src/Pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
-import { Box } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import Card from "../../components/CountUpCard";
+import { BarChart } from "@mui/x-charts/BarChart";
 
 const Dashboard = () => {
   return (
@@ -9,12 +10,26 @@ const Dashboard = () => {
         pb: "3rem",
       }}
     >
-      <div style={{ display: "flex", justifyContent: "space-evenly" }}>
-        <Card count={100} title="Total Active Vacancies" />
-        <Card count={25} title="Pending to Review" />
-        <Card count={25} title="Pending to Review" />
-        <Card count={25} title="Pending to Review" />
-      </div>
+      <Container maxWidth="lg">
+        <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+          <Card count={100} title="Total Active Vacancies" />
+          <Card count={25} title="Pending to Review" />
+          <Card count={25} title="Pending to Review" />
+          <Card count={25} title="Pending to Review" />
+        </div>
+        <BarChart
+          xAxis={[
+            { scaleType: "band", data: ["group A", "group B", "group C"] },
+          ]}
+          series={[
+            { data: [4, 3, 5] },
+            { data: [1, 6, 3] },
+            { data: [2, 5, 6] },
+          ]}
+          width={500}
+          height={300}
+        />
+      </Container>
     </Box>
   );
 };
