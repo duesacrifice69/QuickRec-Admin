b16f705713b9bd6d392a6b69189407f4ade1e809
duesacrifice <chirathtimodh@gmail.com>
2023-11-03 16:14:07 +0530

    list item stylings improved

--- client/src/components/ListItemStyle.jsx
+++ client/src/components/ListItemStyle.jsx
@@ -2,7 +2,15 @@ import { KeyboardArrowDown } from "@mui/icons-material";
 import { Fade, ListItem, ListItemButton, Menu, MenuItem } from "@mui/material";
 import { useState } from "react";
 
-const ListItemStyle = ({ index, icon, active, onClick, subMenu, children }) => {
+const ListItemStyle = ({
+  index,
+  icon,
+  active,
+  sx,
+  onClick,
+  subMenu,
+  children,
+}) => {
   const [anchorEl, setAnchorEl] = useState(false);
   const open = Boolean(anchorEl);
 
@@ -19,6 +27,7 @@ const ListItemStyle = ({ index, icon, active, onClick, subMenu, children }) => {
       role="none"
       sx={{
         width: "max-content",
+        ...sx,
       }}
     >
       <ListItemButton
@@ -30,8 +39,8 @@ const ListItemStyle = ({ index, icon, active, onClick, subMenu, children }) => {
           position: "relative",
           height: "50px",
           boxSizing: "border-box",
-          color: (theme) =>
-            active === index ? theme.palette.primary[100] : "#ffffff",
+          backgroundColor: (theme) =>
+            active === index ? theme.palette.primary[600] : "initial",
           fontWeight: { sm: 600, xs: 400 },
           fontSize: { sm: "1.25rem", xs: "1rem" },
           "&:hover": {
@@ -40,32 +49,36 @@ const ListItemStyle = ({ index, icon, active, onClick, subMenu, children }) => {
           },
         }}
       >
-        {icon}
-        {icon && <div style={{ width: "0.5rem" }}></div>}
+        {icon}&nbsp;&nbsp;
         {children}
-        {subMenu && <KeyboardArrowDown fontSize="20px" sx={{ ml: "0.5rem" }} />}
         {subMenu && (
-          <Menu
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleMenuClose}
-            TransitionComponent={Fade}
-          >
-            {subMenu.map((item, i) => (
-              <MenuItem
-                key={i}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  setAnchorEl(null);
-                  item.onClick(e);
-                }}
-              >
-                {item?.icon}
-                {item?.icon && <div style={{ width: "0.5rem" }}></div>}
-                {item.name}
-              </MenuItem>
-            ))}
-          </Menu>
+          <>
+            &nbsp;
+            <KeyboardArrowDown
+              fontSize="20px"
+              sx={{ display: { sm: "initial", xs: "none" } }}
+            />
+            <Menu
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleMenuClose}
+              TransitionComponent={Fade}
+            >
+              {subMenu.map((item, i) => (
+                <MenuItem
+                  key={i}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setAnchorEl(null);
+                    item.onClick(e);
+                  }}
+                >
+                  {item.icon}&nbsp;&nbsp;
+                  {item.name}
+                </MenuItem>
+              ))}
+            </Menu>
+          </>
         )}
       </ListItemButton>
     </ListItem>
