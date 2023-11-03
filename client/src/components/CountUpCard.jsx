e56a226ba0e69671bf621e3e5643b32c4107a20d
duesacrifice <chirathtimodh@gmail.com>
2023-11-03 16:13:09 +0530

    responsiveness improved

--- client/src/components/CountUpCard.jsx
+++ client/src/components/CountUpCard.jsx
@@ -7,24 +7,31 @@ const CountUpCard = ({ count, title }) => {
     <Box
       sx={{
         backgroundColor: "#ffffff",
+        m: "auto",
         borderRadius: "10px",
         borderTopRightRadius: "30px",
         borderBottomLeftRadius: "30px",
-        width: "200px",
+        // width: { sm: "200px", xs: "125px" },
+        maxWidth: "250px",
         textAlign: "center",
         padding: "1rem 0.75rem",
         boxShadow: (theme) => `10px 10px 10px ${theme.palette.secondary.main}`,
       }}
     >
-      <CountUp
-        style={{
-          color: theme.palette.primary[500],
-          fontWeight: 900,
-          fontSize: "2rem",
+      <Typography
+        sx={{
+          fontSize: { sm: "2rem", xs: "1.5rem" },
         }}
-        end={count}
-        delay={0}
-      />
+      >
+        <CountUp
+          style={{
+            color: theme.palette.primary[500],
+            fontWeight: 900,
+          }}
+          end={count}
+          delay={0}
+        />
+      </Typography>
       <Typography sx={{ fontWeight: 600 }}>{title}</Typography>
     </Box>
   );
