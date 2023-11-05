5eb8cbe58afa8ad74b6a6fda1ee978af288b5586
duesacrifice69 <chirathtimodh@gmail.com>
2023-11-05 17:05:37 +0530

    Fade in transition added to the Error Comp

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -1,4 +1,4 @@
-import { Grid, Typography } from "@mui/material";
+import { Fade, Grid, Typography } from "@mui/material";
 import { useEffect } from "react";
 
 const Error = ({ error, setError }) => {
@@ -8,21 +8,22 @@ const Error = ({ error, setError }) => {
     }, 5000);
   }, [error, setError]);
   return (
-    <Grid item xs={12}>
-      <Typography
-        sx={{
-          display: !error && "none",
-          fontSize: "0.8rem",
-          m: "1rem auto",
-          p: "1rem",
-          color: "#ff0000",
-          border: "1px solid #ff0000",
-          borderRadius: "5px",
-        }}
-      >
-        {error?.message}
-      </Typography>
-    </Grid>
+    <Fade in={error}>
+      <Grid item xs={12}>
+        <Typography
+          sx={{
+            fontSize: "0.8rem",
+            m: "auto",
+            p: "1rem",
+            color: "#ff0000",
+            border: "1px solid #ff0000",
+            borderRadius: "5px",
+          }}
+        >
+          {error}
+        </Typography>
+      </Grid>
+    </Fade>
   );
 };
 
