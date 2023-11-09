d1b519c699cf1f88d1ae8a489db5038ca38f0dbc
duesacrifice <chirathtimodh@gmail.com>
2023-11-09 12:59:21 +0530

    userHasPermission function added

--- client/src/Pages/Profile/Profile.jsx
+++ client/src/Pages/Profile/Profile.jsx
@@ -1,58 +1,18 @@
 import { useEffect, useState } from "react";
-import { Box, Button, Paper, Typography } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { useOutletContext } from "react-router-dom";
-import ProfileAvatar from "../../components/ProfileAvatar";
-import Input from "../../components/Input";
-import ButtonComp from "../../components/ButtonComp";
-import * as API from "../../api/index.js";
-import Error from "../../components/Error";
+import ChangePassword from "./ChangePassword";
 import { useSelector } from "react-redux";
-import permissions from "../../permissions.js";
+import ManageUsers from "./ManageUsers";
+import userHasPermission from "../../permissions";
 
-const initState = {
-  currentPassword: "",
-  newPassword: "",
-  confirmNewPassword: "",
-};
-
-const sections = ["View Profile", "Change Password", "Manage Users"];
+const sections = ["Change Password", "Manage Users"];
 
 const Profile = () => {
   const [setActive] = useOutletContext();
-  const { UserId, UserRole } = useSelector(
-    (state) => state.userContext.data.result
-  );
+  const { UserRole } = useSelector((state) => state.userContext.data.result);
   const [activeSection, setActiveSection] = useState(0);
-  const [error, setError] = useState();
-  const [passwordChangeData, setPasswordChangeData] = useState(initState);
-  const confirmPasswordMatches =
-    passwordChangeData.confirmNewPassword.length === 0 ||
-    passwordChangeData.newPassword === passwordChangeData.confirmNewPassword;
-
   useEffect(() => setActive("4"), [setActive]);
-
-  const handleChange = (e) => {
-    setPasswordChangeData({
-      ...passwordChangeData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await API.changePassword({
-        userId: UserId,
-        currentPassword: passwordChangeData.currentPassword,
-        newPassword: passwordChangeData.newPassword,
-      });
-      setError(response?.message);
-      setPasswordChangeData(initState);
-    } catch (error) {
-      console.log(error?.response?.data);
-      setError(error?.response?.data?.message);
-    }
-  };
 
   return (
     <Box
@@ -66,17 +26,14 @@ const Profile = () => {
         sx={{
           display: "flex",
           flexDirection: "column",
-          minWidth: "200px",
+          minWidth: "250px",
           minHeight: "calc(100vh - 130px)",
           backgroundColor: (theme) => theme.palette.primary[400],
         }}
       >
         {sections
-          .filter(
-            (section) =>
-              !permissions
-                .find((user) => user.role === UserRole)
-                .profile.includes(section)
+          .filter((section) =>
+            userHasPermission({ userRole: UserRole, permission: section })
           )
           .map((section, i) => (
             <Box sx={{ display: "flex" }} key={i}>
@@ -114,72 +71,8 @@ const Profile = () => {
             </Box>
           ))}
       </Box>
-      {activeSection === 0 && (
-        <Box>
-          <ProfileAvatar />
-          <Typography>View Profile</Typography>
-        </Box>
-      )}
-      {activeSection === 1 && (
-        <Paper
-          sx={{
-            m: "1rem 2rem",
-            p: "1.5rem",
-            width: "100%",
-          }}
-          component="form"
-          onSubmit={handleSubmit}
-        >
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "2rem",
-              maxWidth: "400px",
-            }}
-          >
-            <Input
-              label="Current Password *"
-              type="password"
-              name="currentPassword"
-              value={passwordChangeData.currentPassword}
-              handleChange={handleChange}
-              required
-            />
-            <Input
-              label="New Password *"
-              type="password"
-              name="newPassword"
-              value={passwordChangeData.newPassword}
-              handleChange={handleChange}
-              required
-            />
-            <Input
-              label="Confirm New Password *"
-              type="password"
-              name="confirmNewPassword"
-              value={passwordChangeData.confirmNewPassword}
-              handleChange={handleChange}
-              error={!confirmPasswordMatches}
-              helperText={!confirmPasswordMatches && "Password doesn't match."}
-              required
-            />
-          </Box>
-          <ButtonComp
-            type="submit"
-            disabled={!confirmPasswordMatches}
-            sx={{ display: "block", p: "1rem", m: "3rem auto" }}
-          >
-            Change Password
-          </ButtonComp>
-          <Error error={error} setError={setError} />
-        </Paper>
-      )}
-      {activeSection === 2 && (
-        <Box>
-          <Typography>Manage Users</Typography>
-        </Box>
-      )}
+      {activeSection === 0 && <ChangePassword />}
+      {activeSection === 1 && <ManageUsers />}
     </Box>
   );
 };
