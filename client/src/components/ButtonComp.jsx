603302d4cb61f031f2b134629dee28142424b082
duesacrifice <chirathtimodh@gmail.com>
2023-12-14 11:37:59 +0530

    loadingIndicator changed

--- client/src/components/ButtonComp.jsx
+++ client/src/components/ButtonComp.jsx
@@ -1,35 +1,37 @@
-import { LoadingButton } from "@mui/lab";
-import { Box } from "@mui/material";
+import { Box, Button, LinearProgress } from "@mui/material";
 
-const ButtonComp = (props) => {
-  return (
-    <Box textAlign={props.align}>
-      <LoadingButton
-        variant="contained"
-        {...props}
-        loadingPosition="end"
-        sx={{
-          color: (theme) =>
-            theme.palette.getContrastText(theme.palette.primary[500]),
-          backgroundColor: (theme) => theme.palette.primary[500],
-          "&:hover": {
-            backgroundColor: (theme) => theme.palette.primary[400],
-          },
-          ...props.sx,
-        }}
-      >
-        <Box
-          component="span"
-          sx={
-            !props.fullWidth &&
-            props.loading !== undefined &&
-            (props.loading ? { pr: "1rem" } : { px: "0.5rem" })
-          }
-        >
-          {props.children}
-        </Box>
-      </LoadingButton>
-    </Box>
-  );
-};
+const ButtonComp = ({ loading, ...props }) => (
+  <Box textAlign={props.align}>
+    <Button
+      variant="contained"
+      {...props}
+      disabled={loading || props.disabled}
+      sx={{
+        overflow: "hidden",
+        position: "relative",
+        color: (theme) =>
+          theme.palette.getContrastText(theme.palette.primary[500]),
+        backgroundColor: (theme) => theme.palette.primary[500],
+        "&:hover": {
+          backgroundColor: (theme) => theme.palette.primary[400],
+        },
+        display: "inline-flex",
+        ...props.sx,
+      }}
+    >
+      <Box component="span">{props.children}</Box>
+      {loading && (
+        <LinearProgress
+          sx={{
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            width: "100%",
+          }}
+        />
+      )}
+    </Button>
+  </Box>
+);
+
 export default ButtonComp;
